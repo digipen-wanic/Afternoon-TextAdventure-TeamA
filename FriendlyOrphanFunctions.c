ec845343c4c4bbb81eec884853a8788a1d0de93d
PowerUser00 <blakelysnorth@gmail.com>
2019-03-20 17:37:21 -0700

    Finish hostile and friendly orphans

--- FriendlyOrphanFunctions.c
+++ FriendlyOrphanFunctions.c
@@ -22,7 +22,64 @@ void FOrphan_Talk(CommandContext context, GameState* gameState, WorldData* world
 	UNREFERENCED_PARAMETER(context);
 	UNREFERENCED_PARAMETER(gameState);
 	UNREFERENCED_PARAMETER(worldData);
-	
+	Room *room;				 /* The current room */
+	ItemList **roomItemsPtr; /* The list of items in the current room */
+	Item *teddy;			 /* The teddy in the player's inventory */
+
+	/* safety check on the parameters */
+	if ((gameState == NULL) || (worldData == NULL))
+	{
+		return; /* take no action if the parameters are invalid */
+	}
+
+	/* check if we're in the right room to use the item */
+	if (gameState->currentRoomIndex != 7)
+	{
+		/* we are not in the right room - inform the user of the problem and take no action */
+		printf("TODO\n");
+		return;
+	}
+
+	/* check if the cage has already been broken and scored */
+	if (ItemList_FindItem(gameState->inventory, "teddy") == NULL)
+	{
+		/* the player already used the teddy - inform the user of the problem and take no action */
+		printf("TODO\n");
+		return;
+	}
+	else
+	{
+		/* get the current room */
+		room = WorldData_GetRoom(worldData, gameState->currentRoomIndex);
+
+		/* get the list of items in the current room */
+		roomItemsPtr = Room_GetItemList(room);
+		if (roomItemsPtr == NULL)
+		{
+			return; /* take no action, as something is wrong - we should always have an item list */
+		}
+
+		/* Find the teddy in the player's inventory - it should be there, since we are in the Inventory context */
+		teddy = ItemList_FindItem(gameState->inventory, "teddy");
+
+		/* Remove the teddy from the user's inventory - they won't need it again */
+		gameState->inventory = ItemList_Remove(gameState->inventory, teddy);
+
+		/* Tell the user what they did */
+		printf("TODO\n");
+
+		/* Add to the player's score */
+		GameState_ChangeScore(gameState, 10);
+
+		/* Update the room description to reflect the change in the room */
+		//Room_SetDescription(room, "TODO\n");
+
+		/* Add a key to the current room, since the cage has been bashed open */
+		//*roomItemsPtr = ItemList_Add(*roomItemsPtr, Key_Build());
+
+		/* the gold piece has not been scored, so mark the flag */
+		gameState->inventory = ItemList_Add(gameState->inventory, Key_Build());
+	}
 }
 
 /* Build a "FriendlyOrphan" object */
--- HostileOrphanFunctions.c
+++ HostileOrphanFunctions.c
@@ -15,15 +15,83 @@ This file defines the functions to create a specific item, the "HostileOrphan".
 #include "Room.h" /* Room_GetItemList, Room_SetDescription */
 #include "ItemList.h" /* ItemList_FindItem, ItemList_Remove, ItemList_Add */
 #include "Item.h" /* Item_Create */
-
+#include "KeyFunctions.h"
 void HostileOrphan_Talk(CommandContext context, GameState* gameState, WorldData* worldData)
 {
 	/*TODO: Orphan talk*/
 	UNREFERENCED_PARAMETER(context);
 	UNREFERENCED_PARAMETER(gameState);
 	UNREFERENCED_PARAMETER(worldData);
+	Room *room;				 /* The current room */
+	ItemList **roomItemsPtr; /* The list of items in the current room */
+	Item *candy;			 /* The candy in the player's inventory */
+
+	/* safety check on the parameters */
+	if ((gameState == NULL) || (worldData == NULL))
+	{
+		return; /* take no action if the parameters are invalid */
+	}
+
+
+
+	/* check if we're in the right room to use the item */
+	if (gameState->currentRoomIndex != 10)
+	{
+		/* we are not in the right room - inform the user of the problem and take no action */
+		printf("TODO\n");
+		return;
+	}
+
+	/* check if the cage has already been broken and scored */
+	if (ItemList_FindItem(gameState->inventory, "candy") == NULL)
+	{
+		/* the player already used the candy - inform the user of the problem and take no action */
+		printf("TODO\n");
+		return;
+	}
+	else
+	{
+		/* get the current room */
+		room = WorldData_GetRoom(worldData, gameState->currentRoomIndex);
+		
+		/* get the list of items in the current room */
+		roomItemsPtr = Room_GetItemList(room);
+		if (roomItemsPtr == NULL)
+		{
+			return; /* take no action, as something is wrong - we should always have an item list */
+		}
+
+		/* Find the candy in the player's inventory - it should be there, since we are in the Inventory context */
+		candy = ItemList_FindItem(gameState->inventory, "candy");
+
+		/* Remove the candy from the user's inventory - they won't need it again */
+		gameState->inventory = ItemList_Remove(gameState->inventory, candy);
+
+		/* Tell the user what they did */
+		printf("TODO\n");
+
+		/* Add to the player's score */
+		GameState_ChangeScore(gameState, 10);
+
+		/* Update the room description to reflect the change in the room */
+		/* Room_SetDescription(room, "TODO\n");
+
+		/* Add a key to the current room, since the cage has been bashed open */
+		/* *roomItemsPtr = ItemList_Add(*roomItemsPtr, Key_Build());
+
+		/* the gold piece has not been scored, so mark the flag */
+
+		/* 
+		 * throw flag to remove orphan
+		 * change room description to not mention the hostile orphan
+		 * remove candy vrom inv
+		 */
+		ItemList_Remove(gameState->inventory, candy);
+		Room_SetDescription(room, "TODO");
+		gameState->gameFlags = GameFlags_Add(gameState->gameFlags, "orphanGone");
+	}
 }
-/* Build a "brick" object */
+/* Build a "candy" object */
 Item* HostileOrphan_Build()
 {
 	return Item_Create("hostileorphan", "TODO: HostileOrphan Description", false, NULL, NULL, NULL);
