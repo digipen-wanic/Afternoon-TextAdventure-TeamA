2fa14f0a1a4e06e7c3424c2cb553c9715910b7f2
Nico Hickman <nico.hickman@gmail.com>
2019-03-22 15:50:15 -0700

    Add files missed from earlier commit

---
+++ Source/StorageRoomDoor.h
@@ -0,0 +1,16 @@
+/******************************************************************************
+filename    StorageRoomDoor.h
+author     Nico Hickman
+DP email    nicholas.hickman@digipen.edu
+
+Brief Description:
+This file declares the functions to create a specific item, the "candy".
+
+******************************************************************************/
+#pragma once
+
+typedef struct Item Item;
+
+
+/* Build a "brick" object */
+Item* SRDoor_Build();
